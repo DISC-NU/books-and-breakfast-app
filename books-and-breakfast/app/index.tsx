d4ba9f9d57899eac9ea8907c097f5c4896aa8b89
Harry Guan <guanhuai2014@gmail.com>
2024-02-29 17:39:49 -0600

    Update dropdown menu implementation
    - Refactor dropdown rendering in index.tsx for improved performance
    - Update package dependencies to latest versions

--- books-and-breakfast/app/index.tsx
+++ books-and-breakfast/app/index.tsx
@@ -1,20 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react'; // Import React and useState hook
+import {View, Text} from 'react-native'
+import { SelectList } from 'react-native-dropdown-select-list';
 
-export default function App() {
+const App = () => {
+  const [selected, setSelected] = useState<string>(""); // Specify the state type as string
+  
+  const data = [
+    { key: '1', value: 'Willard Elementary'},
+    { key: '2', value: 'Dewey Elementary' },
+    { key: '3', value: 'Haven Middle' },
+    { key: '4', value: 'Kingsley Elementary'},
+    { key: '5', value: 'Lincoln Elementary' },
+    { key: '6', value: 'Walker Elementary' },
+    { key: '7', value: 'Washington Elementary' },
+    { key: '8', value: 'Lincolnwood Elementary' },
+  ];
+
   return (
-    <View style={styles.container}>
-      <Text>Open up index.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style = {{paddingHorizontal:20,paddingVertical:50,flex:1}}>
+      <SelectList 
+        setSelected={(val: string) => setSelected(val)} // Annotate the parameter type as string
+        data={data} 
+        inputStyles={{fontSize:14}}
+        save="value"
+        dropdownStyles={{backgroundColor:'#ADD8E6'}}
+    />
     </View>
+    
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App; // Don't forget to export your component
