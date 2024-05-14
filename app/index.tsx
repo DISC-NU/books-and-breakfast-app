78ee13ba7d5ba7aee4bc0d70921366e774f53dfd
michellehwang07 <michellehwang2026@u.northwestern.edu>
2024-05-13 22:45:31 -0500

    Co-authored-by: Zhengxi Zhang <JackyZhang1219@users.noreply.github.com>

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import AppNavigation from './AppNavigation'; //Adjust the import path as necessary
+import AppNavigation from './AppNavigation'; // Adjust the import path as necessary
 
 export default function App() {
   return <AppNavigation />;
