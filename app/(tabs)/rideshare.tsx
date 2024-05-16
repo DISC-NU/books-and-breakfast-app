c147a4ddfe612b2a683e2fd78c64185c517702bd
JackyZhang1219 <zhangzhengxi1219@gmail.com>
2024-05-16 03:14:15 -0500

    chat initial set up

--- app/(tabs)/rideshare.tsx
+++ app/(tabs)/rideshare.tsx
@@ -1,9 +1,53 @@
-import { Text, View } from 'react-native';
+import React, { useCallback, useEffect, useState } from 'react';
+import { GiftedChat } from 'react-native-gifted-chat';
+import Avatar from '../components/chat/Avatar';
 
-export default function Tab() {
+export default function Example() {
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    setMessages([
+      {
+        _id: 1,
+        text: 'Hello developer',
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: 'React Native',
+          avatar:
+            'https://h5p.org/sites/default/files/h5p/content/1209180/images/file-6113d5f8845dc.jpeg',
+        },
+      },
+      {
+        _id: 2,
+        text: 'Hi developer',
+        createdAt: new Date(),
+        user: {
+          _id: 3,
+          name: 'React Native',
+          avatar:
+            'https://h5p.org/sites/default/files/h5p/content/1209180/images/file-6113d5f8845dc.jpeg',
+        },
+      },
+    ]);
+  }, []);
+
+  const onSend = useCallback((messages = []) => {
+    setMessages((previousMessages) => GiftedChat.append(previousMessages, messages));
+    console.log(messages);
+  }, []);
+
   return (
-    <View style={{ justifyContent: 'center', alignItems: 'center', flex: 1 }}>
-      <Text>Tab Rideshare</Text>
-    </View>
+    <GiftedChat
+      messages={messages}
+      onSend={(messages) => onSend(messages)}
+      user={{
+        _id: 1,
+        name: 'Jacky',
+        avatar:
+          'https://h5p.org/sites/default/files/h5p/content/1209180/images/file-6113d5f8845dc.jpeg',
+      }}
+      renderAvatar={(props) => <Avatar {...props} />}
+    />
   );
 }
