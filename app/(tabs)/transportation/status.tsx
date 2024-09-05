289e0ad60c41dd1d0f2eb8f734b52ed2c4f831b7
michellehwang07 <michellehwang2026@u.northwestern.edu>
2024-09-05 00:23:40 -0400

    fized bugs with transportation status and count

--- app/(tabs)/transportation/status.tsx
+++ app/(tabs)/transportation/status.tsx
@@ -57,12 +57,17 @@ export const StatusScreenDetails = ({ day, groupedUsers }) => {
     { key: '0', value: 'Looking for Walking Buddy' },
     { key: '1', value: 'Looking for CTA/Shuttle Buddy' },
     { key: '2', value: 'Looking for Carpool' },
-    { key: '3', value: 'Can Drive People' },
+    { key: '3', value: 'Willing to Drive' },
   ];
+
+  const totalVolunteers = Object.values(groupedUsers).reduce<number>(
+    (sum, array) => sum + (array as any[]).length,
+    0
+  );
 
   return (
     <View style={styles.container}>
-      <StatusHeader day={day} volunteerNum={Object.keys(groupedUsers).length} />
+      <StatusHeader day={day} volunteerNum={totalVolunteers} />
       <View style={styles.section}>
         {statusSelections.map(
           (status, index) =>
@@ -89,6 +94,7 @@ const styles = StyleSheet.create({
   },
   scrollViewContainer: {
     marginBottom: 20,
+    width: '100%',
   },
   headerContainer: {
     padding: 30,
